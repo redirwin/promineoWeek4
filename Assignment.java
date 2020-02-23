1f6d91295db9edfaceff601b885f5a91f8279679
Dave Irwin <david@redirwin.com>
2020-02-23 15:47:15 -0700

    Created assignment file, completed steps 1-3.

---
+++ Assignment.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Week 4 Coding Assignment
+ */
+public class Assignment {
+
+    public static void main(String[] args) {
+
+        // 1. Create an instance of an ArrayList of String called employeeNames.
+        List<String> employeeNames = new ArrayList<String>();
+
+        // 2. Create an instance of a HashSet of Integer called ids.
+        Set<Integer> ids = new HashSet<Integer>();
+
+        // 3. Create an instance of a HashMap of Integer, String called employeeMap.
+        Map<Integer, String> employeeMap = new HashMap<Integer, String>();
+
+        
+    }
+}
