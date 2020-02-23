c0f8c0ce700e897f938e304aaaca194468ce405b
Dave Irwin <david@redirwin.com>
2020-02-23 16:00:49 -0700

    Part 4 completed.

--- Assignment.java
+++ Assignment.java
@@ -5,9 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * Week 4 Coding Assignment
- */
 public class Assignment {
 
     public static void main(String[] args) {
@@ -21,6 +18,23 @@ public class Assignment {
         // 3. Create an instance of a HashMap of Integer, String called employeeMap.
         Map<Integer, String> employeeMap = new HashMap<Integer, String>();
 
+        // 4. Add at least five entries to the employeeNames and ids (make sure both
+        // collections have the same number of entries).
+        employeeNames.add("Homer");
+        employeeNames.add("Marge");
+        employeeNames.add("Lisa");
+        employeeNames.add("Bart");
+        employeeNames.add("Maggie");
         
+        System.out.println(employeeNames);
+
+        ids.add(1);
+        ids.add(2);
+        ids.add(3);
+        ids.add(4);
+        ids.add(5);
+
+        System.out.println(ids);
+
     }
 }
